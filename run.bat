0d65d62f579bc6e604e57870ac4fafa269ada510
Ziya Ali Shamsi <ziyaali.t.shamsi@gmail.com>
2025-06-03 13:37:28 +0530

    Updated code

--- run.bat
+++
@@ -1,2 +0,0 @@
-set NODE_ENV=development
-set MONGODB_URI=mongodb+srv://thetrailblazermavericks1:rxHxvFRhmYvRNt5H@cluster0.myefyr0.mongodb.net/?retryWrites=true^&w=majority^&appName=Cluster0
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    runtimeErrorOverlay(),
+    ...(process.env.NODE_ENV !== "production" &&
+    process.env.REPL_ID !== undefined
+      ? [
+          await import("@replit/vite-plugin-cartographer").then((m) =>
+            m.cartographer(),
+          ),
+        ]
+      : []),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
